72c694de9265582ae00f0e7798572afdfdacc23d
rhaikal <rhaikal101@gmail.com>
2022-06-06 20:42:19 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis necessitatibus enim quos eum optio fugit! Numquam, ea officiis commodi odio atque tenetur enim odit adipisci error omnis, placeat ipsum voluptate!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
