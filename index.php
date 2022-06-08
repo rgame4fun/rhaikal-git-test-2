bd4322da9edf5c014d091eaace19ff55a113ad48
rhaikal <rhaikal101@gmail.com>
2022-06-08 13:50:19 +0700

    mengubah h1 menjadi h3

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>Coba Git Remote</title>
 </head>
 <body>
-    <h1>Hello World!</h1>
+    <h3>Hello World!</h3>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis necessitatibus enim quos eum optio fugit! Numquam, ea officiis commodi odio atque tenetur enim odit adipisci error omnis, placeat ipsum voluptate!</p>
     <ul>
         <li>satu</li>
